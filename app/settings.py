99fec8e21932c93d36bd742f9f4694420931183f
miraculixx <ps@novapp.ch>
2015-07-19 21:19:30 +0200

    remove deprecation warnings

--- app/settings.py
+++ app/settings.py
@@ -102,3 +102,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# settings.py
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+         'null': {
+           'level': 'DEBUG',
+           'class': 'logging.NullHandler',
+         }
+     },
+    'loggers': {
+         'py.warnings': {
+             'propagate': False,
+             'handlers': ['null']
+          }
+     },
+}
